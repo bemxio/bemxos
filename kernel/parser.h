437569b95a8aa913f8a48a688b3e2fef975368fd
Makoto Miyamoto <48812892+MakotoMiyamoto@users.noreply.github.com>
2022-07-15 16:41:09 -0500

    Reformatted comments.
    The documentation for the respective functions and struct have been redone to be more concise and descriptive.

--- kernel/parser.h
+++ kernel/parser.h
@@ -4,29 +4,25 @@
 #include <stddef.h>
 
 /// This container stores a string split up by whitespace
-/// and quotation canaries in order to tokenize the
-/// internal string by allowing it to be treated
-/// as multiple smaller strings by virtue of the
-/// indices provided by the struct.
-///
-/// The `str` member contains the internal string, and
-/// the `indices` member points to an array of `char *`
-/// which point to each token within the internal `str`.
-/// The size attribute describes the size of the array
-/// pointer to by `indices`.
+/// and quotation bodies.
 struct tokenized_string {
+    /// Points to the internal tokenized string
     char* str;
+    /// Points to an array of `size` `char *`s, all of which
+    /// point to the beginning of each subsequent token.
     char** indices;
+    /// The size index of the indices array (the byte size
+    /// is calculated as `size * sizeof *indices).
     size_t size;
 };
 
-/// frees a `tokenized_string` struct and its subsequent member pointers
+/// Frees a `tokenized_string` struct and its subsequent member pointers
 void free_tokenized_string(struct tokenized_string *tsp);
 
 /// Instantiates a `tokenized_string` with the string passed to it.
-/// The string passed to this factory function is a RHS string that
-/// is copied within the function. The `str` member of `tokenized_string`
-/// is the copied string.
+///
+/// `source_str` - a RHS string that whose copy is assigned as the internal
+/// `str` member of the corresponding `tokenized_string`.
 struct tokenized_string *tokenize_string(const char *source_str);
 
 #endif
