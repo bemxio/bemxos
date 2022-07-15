42716b16cc03e3bcd81c2da99b84edb98782e658
Makoto Miyamoto <48812892+MakotoMiyamoto@users.noreply.github.com>
2022-07-15 16:20:37 -0500

    Un-omit tokenized_string deleter
    The tokenized_string struct pointer deleter will be left un-omitted, and the stack value overload of the corresponding function will be omitted entirely. Also added documentation to the respective functions and struct.

--- kernel/parser.h
+++ kernel/parser.h
@@ -3,17 +3,30 @@
 
 #include <stddef.h>
 
+/// This container stores a string split up by whitespace
+/// and quotation canaries in order to tokenize the
+/// internal string by allowing it to be treated
+/// as multiple smaller strings by virtue of the
+/// indices provided by the struct.
+///
+/// The `str` member contains the internal string, and
+/// the `indices` member points to an array of `char *`
+/// which point to each token within the internal `str`.
+/// The size attribute describes the size of the array
+/// pointer to by `indices`.
 struct tokenized_string {
     char* str;
     char** indices;
     size_t size;
 };
 
-/*
+/// frees a `tokenized_string` struct and its subsequent member pointers
 void free_tokenized_string(struct tokenized_string *tsp);
-void free_tokenized_string(struct tokenized_string tsv);
-*/
 
+/// Instantiates a `tokenized_string` with the string passed to it.
+/// The string passed to this factory function is a RHS string that
+/// is copied within the function. The `str` member of `tokenized_string`
+/// is the copied string.
 struct tokenized_string *tokenize_string(const char *source_str);
 
 #endif
