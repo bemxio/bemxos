46dcf637db8469e34f8d76e78a87247e455df547
Makoto Miyamoto <48812892+MakotoMiyamoto@users.noreply.github.com>
2022-07-15 17:00:13 -0500

    docs(kernel): add comments for the argument parser (#1)
    * Un-omit tokenized_string deleter
    
    The tokenized_string struct pointer deleter will be left un-omitted, and the stack value overload of the corresponding function will be omitted entirely. Also added documentation to the respective functions and struct.
    
    * Reformatted comments.
    
    The documentation for the respective functions and struct have been redone to be more concise and descriptive.
    
    * Unecessary comments removed.
    
    Removed unnecessary comments which omitted some function definitions. The free_tokenized_string function is to remain undefined until a `free()` implementation is written.
    
    * refactor(kernel): reformat the comments differently
    
    i like lower-case no punctuation comments :0efe:
    
    Co-authored-by: bemxio <49116762+bemxio@users.noreply.github.com>

--- kernel/parser.h
+++ kernel/parser.h
@@ -3,17 +3,18 @@
 
 #include <stddef.h>
 
+// this container stores a string split up by whitespace and quotation bodies
 struct tokenized_string {
-    char* str;
-    char** indices;
-    size_t size;
+    char* str; // points to the internal tokenized string
+    char** indices; // Points to an array of `size` `char *`s, all of which point to the beginning of each subsequent token
+    size_t size; // the size index of the indices array (the byte size is calculated as `size * sizeof *indices`)
 };
 
-/*
+/// frees a `tokenized_string` struct and its subsequent member pointers
 void free_tokenized_string(struct tokenized_string *tsp);
-void free_tokenized_string(struct tokenized_string tsv);
-*/
 
+/// instantiates a `tokenized_string` with the string passed to it
+/// `source_str` is a RHS string that whose copy is assigned as the internal `str` member of the corresponding `tokenized_string`
 struct tokenized_string *tokenize_string(const char *source_str);
 
 #endif
