0f66e21f7d1fc90795a6064454522934d0532b24
bemxio <bemxiov@protonmail.com>
2022-07-15 16:32:18 +0200

    feat(stdlib): add malloc to the stdlib

--- stdlib/memory.c
+++ stdlib/memory.c
@@ -11,3 +11,25 @@ void memset(u8 *destination, u8 value, u32 amount) {
         *temporary++ = value;
     }
 }
+
+// this should be computed at link time, but a hardcoded value is fine for now
+// remember that the kernel starts at 0x1000 as defined in the Makefile
+u32 free_memory_address = 0x10000;
+
+u32 malloc(u32 size, int align, u32 *physical_address) { // the implementation is just a pointer to some free memory which keeps growing
+    // pages are aligned to 4K, or 0x1000
+    if (align == 1 && (free_memory_address & 0xFFFFF000)) {
+        free_memory_address &= 0xFFFFF000;
+        free_memory_address += 0x1000;
+    }
+    
+    // also save the physical address
+    if (physical_address) {
+        *physical_address = free_memory_address;
+    }
+
+    u32 ret = free_memory_address;
+    free_memory_address += size; // remember to increment the pointer
+
+    return ret;
+}
--- stdlib/memory.h
+++ stdlib/memory.h
@@ -5,5 +5,6 @@
 
 void memcpy(char *destination, char *source, int amount);
 void memset(u8 *destination, u8 value, u32 amount);
+u32 malloc(u32 size, int align, u32 *physical_address);
 
 #endif
