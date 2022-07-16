3f6b8d725d2188217e27555709c02b459cd794cf
bemxio <bemxiov@protonmail.com>
2022-07-16 18:43:03 +0200

    bug(kernel): add _start to the entry point

--- kernel/entry_point.asm
+++ kernel/entry_point.asm
@@ -1,5 +1,8 @@
 [bits 32]
-[extern kernel_main] ; define the calling point
 
-call kernel_main ; calls the C function (we finally don't need to touch assembly)
-jmp $
+global _start;
+
+_start:
+    [extern kernel_main] ; define the calling point
+    call kernel_main ; calls the C function (we finally don't need to touch assembly)
+    jmp $
