6b6c6e7658b2187e7bf85152b58e937133a8e49f
bemxio <bemxiov@protonmail.com>
2022-07-17 01:48:36 +0200

    feat(kernel): modify the echo command

--- kernel/commands.c
+++ kernel/commands.c
@@ -5,12 +5,18 @@ void help(struct tokenized_string *tokens) {
     kprintln("All of the commands:");
 
     kprintln("`help` - shows this list");
-    kprintln("`echo` - prints the first argument that is provided");
+    kprintln("`echo` - prints arguments that were provided for the command");
     kprintln("`clear` - clears the screen");
 }
 
 void echo(struct tokenized_string *tokens) {
-    kprintln(tokens->indices[1]);
+    char **it = tokens->indices;
+    ++it; // so that it skips the first token
+    
+    while (it < tokens->indices + tokens->size) {
+        kprintln(*it);
+        ++it;
+    }
 }
 
 void clear(struct tokenized_string *tokens) {
